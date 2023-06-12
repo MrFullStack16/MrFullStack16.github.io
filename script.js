8b84867953608859e868392f7dd62a7868406f74
TomasArquibola <tomasvarquibola@gmail.com>
2023-06-12 15:45:31 -0300

    nuevas_modificaciones 12/06/2023 en Redes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 window.onload = function() {
   var reproducirAlbumClicsModernos = document.getElementById('reproducirAlbumClicsModernos');
   var reproducirAlbumPianoBar = document.getElementById('reproducirAlbumPianoBar');
+  var reproducirAlbumParteDeLaReligion = document.getElementById('reproducirAlbumParteDeLaReligion')
+  var reproducirAlbumYendoDeLaCamaAlLiving = document.getElementById('reproducirAlbumYendoDeLaCamaAlLiving')
   var audio_PianoBar = new Audio('PianoBar_Album.mp3'); // Reemplaza 'ruta_de_tu_cancion.mp3' con la ruta a tu archivo MP3
   var audio_album = new Audio('album.mp3');
+  var audio_ParteDeLaReligion = new Audio('Charly García - Parte de la religión (1987) (Álbum Completo).mp3')
+  var audio_YendoDeLaCamaAlLiving = new Audio('Pubis Angelical  Yendo De La Cama Al Living.mp3')
 
   reproducirAlbumClicsModernos.onclick = function() {
       if (audio_album.paused) {
@@ -23,4 +27,22 @@ window.onload = function() {
           reproducirAlbumPianoBar.innerHTML = 'Reproducir';
       }
   };
+  reproducirAlbumParteDeLaReligion.onclick = function() {
+    if (audio_ParteDeLaReligion .paused) {
+        audio_ParteDeLaReligion .play();
+        reproducirAlbumParteDeLaReligion.innerHTML = 'Pausar';
+    } else {
+        audio_ParteDeLaReligion .pause();
+        reproducirAlbumParteDeLaReligion.innerHTML = 'Reproducir';
+    }
+};
+reproducirAlbumYendoDeLaCamaAlLiving.onclick = function() {
+    if (audio_YendoDeLaCamaAlLiving.paused) {
+        audio_YendoDeLaCamaAlLiving.play();
+        reproducirAlbumYendoDeLaCamaAlLiving.innerHTML = 'Pausar';
+    } else {
+        audio_YendoDeLaCamaAlLiving.pause();
+        reproducirAlbumYendoDeLaCamaAlLiving.innerHTML = 'Reproducir';
+    }
+};
 };
